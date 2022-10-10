8d7b4b8804c6c3455c85ad880494bcd7c6780c15
Karl Mayer <karlmayer@gmail.com>
2022-10-10 14:30:14 -0400

    Blog go-live; moved from Handlebars to Nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,75 @@
 const { Console } = require("console");
+const moment = require('moment');
+const CleanCSS = require("clean-css");
+const PluginRss = require("@11ty/eleventy-plugin-rss");
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
+moment.locale('en');
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(PluginRss);
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
 
-    eleventyConfig.addPassthroughCopy('css');
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy('resume/resume.yaml');
 
+    eleventyConfig.addWatchTarget('pages/_css');
+
+    eleventyConfig.addFilter('dateIso', date => {
+        return moment(date).toISOString();
+    });
+
+    eleventyConfig.addFilter('dateReadable', date => {
+        return moment(date).utc().format('LL'); // e.g. May 31, 2019
+    });
+
+    eleventyConfig.addFilter('dateInternational', date => {
+        return moment(date).utc().format('YYYY-MM-DD');
+    });
+
+    eleventyConfig.addFilter("cssmin", function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
+
     return {
         dir: {
-            input: "templates",
-            output: "dist"
+            input: "pages",
+            // ⚠️ These values are both relative to your input directory.
+            includes: "_includes",
+            layouts: "_layouts",
+            output: "dist",
+            templateFormats: ["html", "liquid", "njk"]
         },
         passthroughFileCopy: true
     }
+}
+
+function extractExcerpt(article) {
+    if (!article.hasOwnProperty('templateContent')) {
+        console.warn('Failed to extract excerpt: Document has no property "templateContent".');
+        return null;
+    }
+
+    let excerpt = null;
+    const content = article.templateContent;
+
+    // The start and end separators to try and match to extract the excerpt
+    const separatorsList = [
+        { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+        { start: '<p>', end: '</p>' }
+    ];
+
+    separatorsList.some(separators => {
+        const startPosition = content.indexOf(separators.start);
+        const endPosition = content.indexOf(separators.end);
+
+        if (startPosition !== -1 && endPosition !== -1) {
+            excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
+            return true; // Exit out of array loop on first match
+        }
+    });
+
+    return excerpt;
 }
--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Repo for my personal website, [karlmayer.dev](https://karlmayer.dev),
 which includes my blog, digital resume, interests, and links that might wind up useful to someone.
 
 ## Licenses
-#### **source code:** [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)  
-#### **content:**  [![License: CC BY-SA 4.0](https://licensebuttons.net/l/by-sa/4.0/80x15.png)](https://creativecommons.org/licenses/by-sa/4.0/) Creative Commons Attribution-ShareAlike 4.0 International  
-#### **my resume:**: [![License: CC BY-ND 4.0](https://licensebuttons.net/l/by-nd/4.0/80x15.png)](https://creativecommons.org/licenses/by-nd/4.0/) Creative Commons Attribution-NoDerivates 4.0 International
+**source code:** [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)  
+**content:**  [![License: CC BY-SA 4.0](https://licensebuttons.net/l/by-sa/4.0/80x15.png)](https://creativecommons.org/licenses/by-sa/4.0/) Creative Commons Attribution-ShareAlike 4.0 International  
+**my resume:**: [![License: CC BY-ND 4.0](https://licensebuttons.net/l/by-nd/4.0/80x15.png)](https://creativecommons.org/licenses/by-nd/4.0/) Creative Commons Attribution-NoDerivates 4.0 International
