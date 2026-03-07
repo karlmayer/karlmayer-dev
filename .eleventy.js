52676f713868f0a3c3af5a2300feebda63c314c1
Karl Mayer <karlmayer@gmail.com>
2026-03-07 14:35:07 -0500

    Catch up on 4 years of neglect

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
-const { Console } = require("console");
 const moment = require('moment');
 const CleanCSS = require("clean-css");
-const PluginRss = require("@11ty/eleventy-plugin-rss");
-const { EleventyRenderPlugin } = require("@11ty/eleventy");
-const Image = require("@11ty/eleventy-img");
 
 moment.locale('en');
 
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(PluginRss);
+module.exports = async function (eleventyConfig) {
+    const { EleventyRenderPlugin } = await import("@11ty/eleventy");
+    const PluginRss = await import("@11ty/eleventy-plugin-rss");
+    const Image = (await import("@11ty/eleventy-img")).default;
+
+    eleventyConfig.addPlugin(PluginRss.default);
     eleventyConfig.addPlugin(EleventyRenderPlugin);
 
     eleventyConfig.addPassthroughCopy('img');
@@ -35,60 +35,54 @@ module.exports = function (eleventyConfig) {
             .replace('&', '&amp;')
             .replace(/</g, '‹')
             .replace(/>/g, '›');
-
     })
 
     eleventyConfig.addShortcode('excerpt', article => extractExcerpt(article));
-    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-    eleventyConfig.addLiquidShortcode("image", imageShortcode);
-    eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+    eleventyConfig.addAsyncShortcode("image", async function (src, alt, sizes = "100vw") {
+        let path = this.page.inputPath;
+        let dir = path.substring(0, path.lastIndexOf("/") + 1);
+        src = dir + src;
 
-    return {
-        dir: {
-            input: "pages",
-            includes: "_includes",
-            layouts: "_layouts",
-            output: "dist",
-            templateFormats: ["html", "liquid", "njk"]
-        },
-        passthroughFileCopy: true
-    }
-}
-
-async function imageShortcode(src, alt, sizes = "100vw") {
-    let path = this.page.inputPath;
-    let dir = path.substring(0, path.lastIndexOf("/") + 1);
-    src = dir + src;
-
-    if (alt === undefined) {
-        throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-    }
-
-    let metadata = await Image(src, {
-        widths: [320, null],
-        formats: ["webp"],
-        svgShortCircuit: true,
-        outputDir: "./dist/img/",
-        urlPath: "/img/",
-        sharpOptions: {
-            animated: true
+        if (alt === undefined) {
+            throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
         }
-    });
 
-    let lowsrc = metadata.webp[0];
-    let highsrc = metadata.webp[metadata.webp.length - 1];
+        let metadata = await Image(src, {
+            widths: [320, null],
+            formats: ["webp"],
+            svgShortCircuit: true,
+            outputDir: "./dist/img/",
+            urlPath: "/img/",
+            sharpOptions: {
+                animated: true
+            }
+        });
 
-    return `<div class="picture">
+        let lowsrc = metadata.webp[0];
+
+        return `<div class="picture">
     <picture>
       ${Object.values(metadata).map(imageFormat => {
-        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
-    }).join("\n")}
+            return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+        }).join("\n")}
         <img
           src="${lowsrc.url}"
           alt="${alt}"
           loading="lazy"
           decoding="async">
       </picture></div>`;
+    });
+
+    return {
+        dir: {
+            input: "pages",
+            includes: "_includes",
+            layouts: "_layouts",
+            output: "dist"
+        },
+        templateFormats: ["html", "liquid", "njk", "md"],
+        passthroughFileCopy: true
+    }
 }
 
 function extractExcerpt(article) {
@@ -100,7 +94,6 @@ function extractExcerpt(article) {
     let excerpt = null;
     const content = article.templateContent;
 
-    // The start and end separators to try and match to extract the excerpt
     const separatorsList = [
         { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
         { start: '<p>', end: '</p>' }
@@ -112,7 +105,7 @@ function extractExcerpt(article) {
 
         if (startPosition !== -1 && endPosition !== -1) {
             excerpt = content.substring(startPosition + separators.start.length, endPosition).trim();
-            return true; // Exit out of array loop on first match
+            return true;
         }
     });
 
